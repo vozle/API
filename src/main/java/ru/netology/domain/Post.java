aa2a30202498c5a8decc962fb6f7dd13e36b4d54
vozle <vozle2@gmail.com>
2022-03-04 18:37:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
+
+
